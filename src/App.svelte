3a92f75af85ce1b30a018259a2be8ca43944c6b3
daiguuji7 <daiguuji7@gmail.com>
2026-01-28 21:08:57 +0900

    0128

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>import "./app.css";
+import AnswerButton from "./AnswerButton.svelte";
+let currentScore = 0;
+let renzokuSeikai = 0;
+
+function answerButtonClicked(isCorrect){
+    if(isCorrect){
+    renzokuSeikai += 1;
+    currentScore += renzokuSeikai;
+    }
+    else{
+        
+    }
+}</script>
+
+<main class="flex flex-col h-svh">
+    <!-- メニューバー -->
+    <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
+        <div>スコア:{currentScore}</div>
+        <div>連続正解数:{renzokuSeikai}</div>
+    </div>
+  
+    <!-- 問題 -->
+    <div class="bg-green-200 text-center text-4xl py-4">鮪</div>
+    
+    <!-- 選択肢 -->
+    <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
+        <AnswerButton on:click="{answerButtonClicked}">ひらめ</AnswerButton>
+        <AnswerButton on:click="{answerButtonClicked}">かれい</AnswerButton>
+        <AnswerButton on:click="{answerButtonClicked}">まぐろ</AnswerButton>
+        <AnswerButton on:click="{answerButtonClicked}">ぶり</AnswerButton>
+        <AnswerButton on:click="{answerButtonClicked}">くじら</AnswerButton>
+    </div>
+</main>
