372e79cf68c4862ed0bff3f58de65e4549514caf
daiguuji7 <daiguuji7@gmail.com>
2026-01-29 16:03:44 +0900

    タイトル作成

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
 import "./app.css";
 import AnswerButton from "./AnswerButton.svelte";
 import {getQuizdata} from "./quizdataFactory";
+import TitlePage from "./TitlePage.svelte";
+let showTitle = true;
+
 let currentScore = 0;
 let renzokuSeikai = 0;
 let quizdata = getQuizdata();
@@ -21,20 +24,24 @@ const correctAnswerButtonClicked = () => answerButtonClicked(true);
 
 </script>
 
-<main class="flex flex-col h-svh">
-    <!-- メニューバー -->
-    <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
-        <div>スコア:{currentScore}</div>
-        <div>連続正解数:{renzokuSeikai}</div>
-    </div>
-  
-    <!-- 問題 -->
-    <div class="bg-green-200 text-center text-4xl py-4">{quizdata.mondai}</div>
+{#if showTitle}
+    <TitlePage on:click={()=>showTitle=false}></TitlePage>
+{:else}
+    <main class="flex flex-col h-svh">
+        <!-- メニューバー -->
+        <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
+            <div>スコア:{currentScore}</div>
+            <div>連続正解数:{renzokuSeikai}</div>
+        </div>
     
-    <!-- 選択肢 -->
-    <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
-        {#each quizdata.taku as t}
-            <AnswerButton on:click={() => answerButtonClicked(quizdata.seikai===t)}>{t}</AnswerButton>
-        {/each}
-    </div>
-</main>
+        <!-- 問題 -->
+        <div class="bg-green-200 text-center text-4xl py-4">{quizdata.mondai}</div>
+        
+        <!-- 選択肢 -->
+        <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
+            {#each quizdata.taku as t}
+                <AnswerButton on:click={() => answerButtonClicked(quizdata.seikai===t)}>{t}</AnswerButton>
+            {/each}
+        </div>
+    </main>
+{/if}
